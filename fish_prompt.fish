597fc3e4fb060a2d9a1c9a23d7de4621837d6cbf
Heejong Ahn <heejongahn@gmail.com>
2017-01-06 10:47:40 +0900

    Remove virtualenv glyph

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -565,26 +565,9 @@ function __bobthefish_prompt_vi -S -d 'Display vi mode'
   set_color normal
 end
 
-function __bobthefish_virtualenv_python_version -S -d 'Get current python version'
-  switch (python --version ^&1)
-    case 'Python 2*PyPy*'
-      echo $__bobthefish_pypy_glyph
-    case 'Python 3*PyPy*'
-      echo -s $__bobthefish_pypy_glyph $__bobthefish_superscript_glyph[3]
-    case 'Python 2*'
-      echo $__bobthefish_superscript_glyph[2]
-    case 'Python 3*'
-      echo $__bobthefish_superscript_glyph[3]
-  end
-end
-
 function __bobthefish_prompt_virtualfish -S -d "Display activated virtual environment (only for virtualfish, virtualenv's activate.fish changes prompt by itself)"
   [ "$theme_display_virtualenv" = 'no' -o -z "$VIRTUAL_ENV" ]; and return
-  set -l version_glyph (__bobthefish_virtualenv_python_version)
-  if [ "$version_glyph" ]
-    __bobthefish_start_segment $__color_virtualfish
-    echo -ns $__bobthefish_virtualenv_glyph $version_glyph ' '
-  end
+  __bobthefish_start_segment $__color_virtualfish
   echo -ns (basename "$VIRTUAL_ENV") ' '
   set_color normal
 end
